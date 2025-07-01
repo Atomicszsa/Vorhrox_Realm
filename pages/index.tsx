1f43a24476eacca221416912619dda135b2abc93
Atomicszsa <169003977+Atomicszsa@users.noreply.github.com>
2025-07-01 10:12:45 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Vorhrox Realm!</title>
+        <title>Vorhrox Realms!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
       </Head>
